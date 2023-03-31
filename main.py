ac4c4e7a89e3d9ae9c7e480a36255e594914a330
Каирбеков Мадияр Кайратович <kairbekov_m@c.nis.edu.kz>
2023-03-31 16:36:57 +0600

    lesson 4

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from aiogram import Bot, Dispatcher, executor, types
 
 from config import TOKEN_API
 
+import string
+import random
+
 HELP_COMMAND = """
 /help - список команд
 /start - начать работу с ботом
+/description - что умеешь?
 /count - число собственных предыдущих вызовов
 """
 clientBot = Bot(TOKEN_API)
@@ -15,9 +19,24 @@ async  def help_command(message: types.Message):
     await message.reply(text=HELP_COMMAND)
 
 @dp.message_handler(commands=['start'])
-async  def help_command(message: types.Message):
+async  def start_command(message: types.Message):
     await message.answer(text="Добро пожаловать в наш Телеграм Бот!")
     await message.delete()
+
+@dp.message_handler(commands=['count'])
+async  def check_command(message: types.Message):
+    global count
+    await message.answer(f'COUNT: {count}')
+    count += 1
+
+@dp.message_handler(commands=['description'])
+async  def desc_command(message: types.Message):
+    await message.answer('Данный бот умеет отправлять рандомные символы латинского алфавита')
+    await message.delete()
+
+@dp.message_handler()
+async def send_random_letter(message: types.Message):
+    await message.reply(random.choice(string.ascii_letters))
 
 if __name__ == '__main__':
     executor.start_polling(dp)
