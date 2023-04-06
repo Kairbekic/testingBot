802b4389ddc64f1a0b9acfcf83c33077195ac34e
Каирбеков Мадияр Кайратович <kairbekov_m@c.nis.edu.kz>
2023-04-06 12:18:35 +0600

    Added function for working  with GoogleSheets

---
+++ gsheet.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import os.path
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+SAMPLE_RANGE_NAME = 'Test List!A2:E246'
+
+class GoogleSheet:
+    SPREADSHEET_ID = '1xlEgz_yZ9iR2K5F_h9VuqgxRC59nBJvlXi7QXVMyCUs'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    service = None
+
+    def __init__(self):
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                print('flow')
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('sheets', 'v4', credentials=creds)
+
+    def updateRangeValues(self, range, values):
+        data = [{
+            'range': range,
+            'values': values
+        }]
+        body = {
+            'valueInputOption': 'USER_ENTERED',
+            'data': data
+        }
+        result = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.SPREADSHEET_ID,
+                                                                  body=body).execute()
+        print('{0} cells updated.'.format(result.get('totalUpdatedCells')))
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from aiogram.utils import executor
 from create_bot import dp
 from data_base import sqlite_db
+from gsheet import GoogleSheet
 
+def main():
+    gs = GoogleSheet()
+    test_range = 'TestList!F2:G4'
+    test_values = [
+        [11, 22],
+        [32, 41],
+        [13, 95]
+    ]
+    gs.updateRangeValues(test_range, test_values)
 async def on_startup(_):
     print('Бот был успешно запущен!')
     sqlite_db.sql_start()
+    main()
+
 
 from handlers import client, other, admin
 
@@ -13,6 +25,4 @@ admin.register_handlers_admin(dp)
 other.register_handlers_other(dp)
 
 if __name__ == '__main__':
-    executor.start_polling(dp,
-                           on_startup=on_startup,
-                           skip_updates=True)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
